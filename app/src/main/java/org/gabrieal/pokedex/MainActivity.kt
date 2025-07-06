489f64e82f4a63ccaa236d1dec85eea06f7a2f19
Daniel Gabrieal Jayarajan <danieljayarajan@gmail.com>
2025-07-06 15:01:18 +0800

    add pokedex layout

--- app/src/main/java/org/gabrieal/pokedex/MainActivity.kt
+++ app/src/main/java/org/gabrieal/pokedex/MainActivity.kt
@@ -1,17 +1,40 @@
 package org.gabrieal.pokedex
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
+
+    private val llRoot by lazy { findViewById<LinearLayout>(R.id.ll_root) }
+    private val ivPokedex by lazy { findViewById<ImageView>(R.id.iv_pokedex) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        setupInsets()
+        setupViewModel()
+        setupUI()
+    }
+
+    private fun setupUI() {
+
+    }
+
+    private fun setupViewModel() {
+
+
+    }
+
+    private fun setupInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(llRoot) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
